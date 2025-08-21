21dc8f6d3663f4a28d0a8969c517e0f924157be2
LeoniksonN-28 <00001249196322sp@al.educacao.sp.gov.br>
2025-08-21 17:57:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
-
-    botaoDeAcessibilidade.addEventListener('click', function () document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
  
@@ -37,28 +33,10 @@ document.addEventListener('DOMContentLoaded', function () {
      alternaContraste.addEventListener('click', function(){
          document.body.classList.toggle('alto-contraste')
      })
- 
- 
- }){
-        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+  
+ })
 
-    })
-
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-
-    let tamanhoAtualFonte = 1;
-
-    aumentaFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
-    })
-
-    diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
-    })
-})
+ ScrollReveal().reveal('#inicio', { delay: 500 });
+ ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ ScrollReveal().reveal('#galeria', { delay: 500 });
+ ScrollReveal().reveal('#contato', { delay: 500 });
